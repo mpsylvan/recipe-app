0a8677680314b6caeea0fa1ff8c6a5980982fc87
Casey Sabal <caseysabal@Caseys-iMac.lan>
2023-08-28 23:19:34 -0400

    login mixin inherited

--- ingredients/views.py
+++ ingredients/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Ingredient
 # Create your views here.
 
-class IngredientsList(ListView):
+class IngredientsList(LoginRequiredMixin, ListView):
     model = Ingredient
     template_name = 'ingredients/ing.html'
 
-class IngredientsDetail(DetailView):
+class IngredientsDetail(LoginRequiredMixin, DetailView):
     model = Ingredient
     template_name = 'ingredients/ingd.html'
