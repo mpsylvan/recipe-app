ae5fac441d4f573dcab03ff9f2f0e6b3df4a70a4
Casey Sabal <caseysabal@Caseys-iMac.lan>
2023-08-28 23:22:26 -0400

    login auth url and adds top level template to templates directory

--- recipe_app/settings.py
+++ recipe_app/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'recipe_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -102,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_URL = '/login/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
