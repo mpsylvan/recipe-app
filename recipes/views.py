9997f1beb7aa672850aaafd578db997f2a3d01b4
Casey Sabal <caseysabal@Caseys-iMac.lan>
2023-08-28 23:16:26 -0400

    additional recipe views

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Recipe
 
 # Create your views here.
@@ -7,11 +8,13 @@ from .models import Recipe
 def home(request):
     return render(request, 'recipes/recipes_home.html')
 
-class RecipeList(ListView):
+class RecipeList(LoginRequiredMixin, ListView):
     model = Recipe
     template_name = "recipes/recipes_list.html"
 
-class RecipeDetail(DetailView):
+class RecipeDetail(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = "recipes/detail.html"
-    
+
+def records(request):
+    return render(request, 'recipes/records.html')
