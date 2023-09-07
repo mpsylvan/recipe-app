aaf1d375841bb35e20435eceeb51dfcba6912b68
mpsylvan <micperdo@gmail.com>
2023-09-07 17:20:57 -0400

    adds about route and url ref

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home, records, RecipeList, RecipeDetail, RecipeInput
+from .views import home, about, records, RecipeList, RecipeDetail, RecipeInput
 
 app_name = "recipes"
 
 urlpatterns = [
     path("", home, name="home"),
+    path("about", about, name="about"),
     path("list", RecipeList.as_view(), name="recipe-list"),
     path("list/<pk>", RecipeDetail.as_view(), name="detail"),
     path("records", records, name="records"),
