99a291edf8ac1ab4f902674f4aaa278c79c14775
mpsylvan <micperdo@gmail.com>
2023-09-07 00:02:55 -0400

    updates

--- recipe_app/settings.py
+++ recipe_app/settings.py
@@ -21,11 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 
-SECRET_KEY = os.environ.get(
-    "DJANGO_SECRET_KEY",
-    "django-insecure-ml49cp(e)=yakpevh4xz)3w)6xuq6kv7g&3^xf^)gr-n3&p#%9",
-)
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
