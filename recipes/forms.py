45a2e2238acca56f6b838a7a40a922b34489154f
mpsylvan <micperdo@gmail.com>
2023-09-07 19:24:26 -0400

    removes unused choices array

--- recipes/forms.py
+++ recipes/forms.py
@@ -2,23 +2,17 @@ from django import forms
 from .models import Recipe
 from ingredients.models import Ingredient
 
-
+#  tuple of tuples of choices for which type of data is being pulled and used when forming a dataframe and visualization.
 CONTENT_CHOICES = (
     ("#1", "Recipes (cooktime line graph)"),
     ("#2", "Ingredients (frequency wheel)"),
     ("#3", "Difficulty (bar graph)"),
 )
 
-# CHART_CHOICES = (
-#     ("a", "Bar"),
-#     ("b", "Graph"),
-#     ("c", "Line"),
-# )
 
-
+# the name choice will get parsed in the form view to specify data output.
 class RecipeSearchForm(forms.Form):
     name = forms.ChoiceField(choices=CONTENT_CHOICES)
-    # chart_type = forms.ChoiceField(choices=CHART_CHOICES)
     ingredients_limit = forms.IntegerField(
         help_text="(^leave blank to display all recipes, 5 - 20 ingredients)",
         required=False,
